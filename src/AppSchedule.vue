6648fbed009ddf3cc38ae04a40647aad5de12ff0
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2022-04-26 10:02:04 +0200

    Composable fetch + schedule example

---
+++ src/AppSchedule.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { useFetch } from './composables/fetch.js';
+
+  const { data: courses } = useFetch('https://chabloz.eu/files/horaires/all.json');
+
+
+</script>
+
+<template>
+  <h1>Horaires</h1>
+
+  <ul>
+    <li v-for="course of courses">
+      {{ course.label }}
+      à
+      {{ course.start }}
+    </li>
+  </ul>
+</template>
+
+<style>
+
+</style>
--- src/AppTodoList.vue
+++ src/AppTodoList.vue
@@ -1,12 +1,10 @@
 <script setup>
+  import { computed, ref } from 'vue';
   import { useLocalstorage } from './composables/localstorage.js';
   import { tasks } from './state.js';
 
   const { val } = useLocalstorage('foo', 'bar');
   const { val: val2 } = useLocalstorage('foo2', 'bar2');
-
-
-
 
   // setTimeout(() => {
   //   tasks.value.push({
@@ -20,15 +18,28 @@
   //   console.log(tasks.value);
   // }, 1000)
 
+  const filter = ref('test');
+
+  const tasksFiltered = computed(() => {
+    return tasks.value.filter(task => {
+      return task.label.toLowerCase().includes(filter.value.toLowerCase());
+    })
+  })
+
+  const taskSorted = computed(() => {
+    return [...tasksFiltered.value].sort()
+  })
+
 </script>
 
 <template>
   <h1>Todo list</h1>
   <div>{{ val }}</div>
-  <div>{{ val2 }}</div>
+
+  <input v-model="filter" type="text" placeholder="filtrer la todo list">
 
   <ul>
-    <li v-for="(task, index) of tasks">
+    <li v-for="(task, index) of tasksFiltered">
       {{ task.label }}
     </li>
   </ul>
